57269ee343fb8a3a850507acfc21123b83b87ad0
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-02-19 20:08:28 +0200

    Medianos apskaičiavimas
    Pridėtas medianos apskaičiavimo funckija

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <algorithm>
+
 using std::string;
 using std::cin;
 using std::vector;
@@ -12,18 +14,23 @@ using std::setw;
 using std::endl;
 using std::fixed;
 using std::setprecision;
+using std::sort;
+
 
 struct Studentas {
     string vardas, pavarde;
     vector <int> paz;
     int egz;
-    double rez;
+    double vid;
+    double med;
 };
 void inputas(vector <Studentas>& grupe);
+void mediana(vector <Studentas>& grupe);
 void outputas(const vector <Studentas>& grupe);
 int main() {
     vector<Studentas> grupe;
     inputas(grupe);
+    mediana(grupe);
     outputas(grupe);
 }
 
@@ -43,18 +50,31 @@ void inputas(vector <Studentas>& grupe) {
             A.paz.push_back(temp); sum += temp;
         }
         cout << "Iveskite egzamina: "; cin >> A.egz;
-        A.rez = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
+        A.vid = sum * 1 / (n * 1) * 0.4 + A.egz * 0.6;
         grupe.push_back(A);
         A.paz.clear();
     }
 }
 
+void mediana(vector<Studentas>& grupe)
+{
+    for (auto& A : grupe) {
+        int n = A.paz.size();
+       sort(A.paz.begin(), A.paz.end());
 
+        if (n % 2 != 0) {
+            A.med = A.paz[n / 2];
+        }
+        else A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+    }
+
+
+}
 void outputas(const vector<Studentas>& grupe) {
     for (auto A : grupe) {
         cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
         //for(auto k: A.paz) cout<<setw(3)<<k;
         //cout<<setw(5)<<A.egz;
-        cout << setw(10) << fixed << setprecision(2) << A.rez;
+        cout << setw(10) << fixed << setprecision(2) << A.vid <<" "<<A.med;
     }
 }
