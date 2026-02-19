f8606a74835cc095d09be16da8811d676b3b9cef
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-02-19 22:19:33 +0200

    Ranka irasomi duomenys
    Prideta funkcija, jog duomenys butu irasomi ranka.

--- Vektoriai.cpp
+++ Vektoriai.cpp
@@ -24,51 +24,52 @@ struct Studentas {
     double vid;
     double med;
 };
-void inputas(vector <Studentas>& grupe, char& c);
-void mediana(vector <Studentas>& grupe);
-void outputas(const vector <Studentas>& grupe, char c);
+
+void inranka(vector <Studentas>& grupe);
+void outputas(const vector <Studentas>& grupe, char& rez);
+
 int main() {
     vector<Studentas> grupe;
-    char c;
-    inputas(grupe, c);
-    mediana(grupe);
-    outputas(grupe, c);
+    char rez;
+    inranka(grupe);
+    outputas(grupe, rez);
 }
 
-void inputas(vector <Studentas>& grupe, char& c) {
+void inranka(vector <Studentas>& grupe) {
 
-    for (int ii = 0; ii < 2; ii++) {
+    while (true) {
         Studentas A;
-        cout << "Iveskite studento vardo ir pavarde ";
+        int sum = 0;
+        cout << "Iveskite studento vardo ir pavarde. Norint baigti studentu ivedima, iveskite 0: ";
         cin >> A.vardas >> A.pavarde;
-        cout << "Iveskite semestro ivercius. Kiek ju bus? ";
-        int n, sum = 0;
-        while(true){
-            if (cin >> n && n >= 0) {
-                break;
+
+        if (A.vardas == "0" || A.pavarde == "0") {
+            break;
+        }
+        int temp;
+        cout << "Iveskite pazymius nuo 1 iki 10. Norint baigti pazymiu ivedima, iveskite 0: ";
+
+        while (true) {
+            if (cin >> temp) {
+                if (temp == 0) {
+                    break;
+                }
+                if (temp >= 1 && temp <= 10) {
+                    A.paz.push_back(temp);
+                    sum += temp;
+                }
+                else {
+                    cout << "Neteisingas pazymys, iveskite 1-10 arba 0: ";
+                }
             }
             else {
-                cout << "Neteisingas ivedimas, iveskite teigiama skaiciu: ";
+                cout << "Neteisingas ivedimas, iveskite skaiciu: ";
                 cin.clear();
                 cin.ignore(10000, '\n');
             }
         }
-        for (int i = 0; i < n; i++) {
-            int temp;
-            cout << "Iveskite " << i + 1 << " pazymi is " << n << ": ";
-            while (true) {
-                if (cin >> temp && temp >= 1 && temp <= 10) {
-                    break;
-                }
-                else {
-                    cout << "Neteisingas ivedimas, iveskite skaiciu nuo 1 iki 10: ";
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                }
-            }
-            A.paz.push_back(temp); sum += temp;
-        }
-        cout << "Iveskite egzamina: ";
+
+        cout << "Iveskite egzamino rezultata: ";
         while (true) {
             if (cin >> A.egz && A.egz >= 1 && A.egz <= 10) {
                 break;
@@ -79,13 +80,33 @@ void inputas(vector <Studentas>& grupe, char& c) {
                 cin.ignore(10000, '\n');
             }
         }
+        int n = A.paz.size();
         A.vid = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
-        grupe.push_back(A);
         A.paz.clear();
+
+        for (auto& A : grupe) {
+            int n = A.paz.size();
+            sort(A.paz.begin(), A.paz.end());
+
+            if (n % 2 != 0) {
+                A.med = A.paz[n / 2];
+                A.med = A.paz[n / 2];
+            }
+            else if(n > 0){
+                A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+                A.med = A.med * 0.4 + A.egz * 0.6;
+
+            }
+
+        }
+        grupe.push_back(A);
     }
+}
+void outputas(const vector<Studentas>& grupe, char &rez) {
     cout << "Rezultata isvesti su mediana(irasyti M arba m) ar vidurkiu(irasyti V arba v)";
+    cin >> rez;
     while (true) {
-        if (cin >> c && c == 'V' || c == 'v' || c == 'M' || c == 'm') {
+        if (rez == 'V' || rez == 'v' || rez == 'M' || rez == 'm') {
             break;
         }
         else {
@@ -94,25 +115,7 @@ void inputas(vector <Studentas>& grupe, char& c) {
             cin.ignore(10000, '\n');
         }
     }
-
-}
-
-void mediana(vector<Studentas>& grupe)
-{
-    for (auto& A : grupe) {
-        int n = A.paz.size();
-       sort(A.paz.begin(), A.paz.end());
-
-       if (n % 2 != 0) {
-           A.med = A.paz[n / 2];
-       }
-       else A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
-    }
-
-
-}
-void outputas(const vector<Studentas>& grupe, char c) {;
-    if (c == 'V' || c == 'v') {
+    if (rez == 'V' || rez == 'v') {
         cout << left << setw(10) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(10) << "vidurkis" << endl;
         for (auto A : grupe) {
 
@@ -120,13 +123,13 @@ void outputas(const vector<Studentas>& grupe, char c) {;
             cout << left << setw(10) << fixed << setprecision(2) << A.vid << endl;
         }
     }
-    if (c == 'M' || c == 'm') {
+    if (rez == 'M' || rez == 'm') {
         cout << left << setw(10) << "Vardas" << left << setw(20) << "Pavarde" << left << setw(10) << "mediana" << endl;
 
         for (auto A : grupe) {
 
             cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
-            cout << setw(10) << fixed << setprecision(2) << A.med<<endl;
+            cout << setw(10) << fixed << setprecision(2) << A.med << endl;
         }
     }
 }
