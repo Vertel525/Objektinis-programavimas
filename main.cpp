0965b9daf436fa6afca992039276910ccd038971
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-02-19 20:25:52 +0200

    Rezultato pasirinkimas tarp vidurkio ar medianos
    Pridėta galimybė pasirinkt, kad rezultatas būtų išvestas vidurkiu arba mediana.

--- main.cpp
+++ main.cpp
@@ -24,17 +24,18 @@ struct Studentas {
     double vid;
     double med;
 };
-void inputas(vector <Studentas>& grupe);
+void inputas(vector <Studentas>& grupe, char& c);
 void mediana(vector <Studentas>& grupe);
-void outputas(const vector <Studentas>& grupe);
+void outputas(const vector <Studentas>& grupe, char c);
 int main() {
     vector<Studentas> grupe;
-    inputas(grupe);
+    char c;
+    inputas(grupe, c);
     mediana(grupe);
-    outputas(grupe);
+    outputas(grupe, c);
 }
 
-void inputas(vector <Studentas>& grupe) {
+void inputas(vector <Studentas>& grupe, char& c) {
 
     for (int ii = 0; ii < 2; ii++) {
         Studentas A;
@@ -50,10 +51,13 @@ void inputas(vector <Studentas>& grupe) {
             A.paz.push_back(temp); sum += temp;
         }
         cout << "Iveskite egzamina: "; cin >> A.egz;
-        A.vid = sum * 1 / (n * 1) * 0.4 + A.egz * 0.6;
+        A.vid = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
         grupe.push_back(A);
         A.paz.clear();
     }
+    cout << "Rezultata isvesti su mediana(irašyti M arba m) ar vidurkiu(irašyti V arba v)";
+    cin >> c;
+
 }
 
 void mediana(vector<Studentas>& grupe)
@@ -62,19 +66,27 @@ void mediana(vector<Studentas>& grupe)
         int n = A.paz.size();
        sort(A.paz.begin(), A.paz.end());
 
-        if (n % 2 != 0) {
-            A.med = A.paz[n / 2];
-        }
-        else A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+       if (n % 2 != 0) {
+           A.med = A.paz[n / 2];
+       }
+       else A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
     }
 
 
 }
-void outputas(const vector<Studentas>& grupe) {
-    for (auto A : grupe) {
-        cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
-        //for(auto k: A.paz) cout<<setw(3)<<k;
-        //cout<<setw(5)<<A.egz;
-        cout << setw(10) << fixed << setprecision(2) << A.vid <<" "<<A.med;
+void outputas(const vector<Studentas>& grupe, char c) {
+    if (c == 'V' || c == 'v') {
+        for (auto A : grupe) {
+
+            cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
+            cout << setw(10) << fixed << setprecision(2) << A.vid << endl;
+        }
+    }
+    if (c == 'M' || c == 'm') {
+        for (auto A : grupe) {
+
+            cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
+            cout << setw(10) << fixed << setprecision(2) << A.med<<endl;
+        }
     }
 }
