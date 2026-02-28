1773787eb83eb38a69bbb482eb0af1cd8ebe033d
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-02-28 19:25:20 +0200

    Duomenu nuskaitymas is failu
    Prideta galimybe nuskaityti duomenis ia failo

--- Vektoriai.cpp
+++ Vektoriai.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
+#include <fstream>
 
 using std::string;
 using std::cin;
@@ -17,6 +19,7 @@ using std::endl;
 using std::fixed;
 using std::setprecision;
 using std::sort;
+using std::ifstream;
 
 
 struct Studentas {
@@ -34,6 +37,7 @@ void inranka(vector <Studentas>& grupe);
 void randpazymiai(vector <Studentas>& grupe);
 void random(vector <Studentas>& grupe);
 void outputas(const vector <Studentas>& grupe, char& rez);
+void skaityti(vector <Studentas>& grupe);
 
 int main() {
     srand(time(0));
@@ -45,23 +49,27 @@ int main() {
     cout << " 1 - ivedimas ranka" << endl;
     cout << " 2 - generuojami pazymiai" << endl;
     cout << " 3 - generuojami studentai ir pazymiai" << endl;
-    cout << " 4 - baigti darba ";
+    cout << " 4 - baigti darba " << endl;
+    cout << " 5 - nuskaityti failus";
 
-        cin >> pasirinkimas;
-        if (pasirinkimas == 1) {
-            inranka(grupe);
-        }
-        if (pasirinkimas == 2) {
-            randpazymiai(grupe);
-        }
-        if (pasirinkimas == 3) {
-            random(grupe);
-        }
-        if (pasirinkimas == 4) {
-            return 0;
-        }
+    cin >> pasirinkimas;
+    if (pasirinkimas == 1) {
+        inranka(grupe);
+    }
+    if (pasirinkimas == 2) {
+        randpazymiai(grupe);
+    }
+    if (pasirinkimas == 3) {
+        random(grupe);
+    }
+    if (pasirinkimas == 4) {
+        return 0;
+    }
+    if (pasirinkimas == 5) {
+        skaityti(grupe);
+    }
 
-    
+
     outputas(grupe, rez);
 }
 
@@ -126,9 +134,9 @@ void inranka(vector <Studentas>& grupe) {
 
         }
 
-    grupe.push_back(A);
-    } 
-    
+        grupe.push_back(A);
+    }
+
 }
 
 void randpazymiai(vector <Studentas>& grupe) {
@@ -169,58 +177,58 @@ void randpazymiai(vector <Studentas>& grupe) {
             A.med = A.med * 0.4 + A.egz * 0.6;
 
         }
-    grupe.push_back(A);
+        grupe.push_back(A);
     }
 }
 
 
 void random(vector <Studentas>& grupe) {
 
-        int kiek1;
-        cout << "Kiek studentu sugeneruoti? " << endl;
-        cin >> kiek1;
+    int kiek1;
+    cout << "Kiek studentu sugeneruoti? " << endl;
+    cin >> kiek1;
 
-        for (int i = 0; i < kiek1; i++) {
-            Studentas A;
-            int sum = 0;
-            A.vardas = vardai[rand() % vardai.size()];
-            A.pavarde = pavardes[rand() % pavardes.size()];
+    for (int i = 0; i < kiek1; i++) {
+        Studentas A;
+        int sum = 0;
+        A.vardas = vardai[rand() % vardai.size()];
+        A.pavarde = pavardes[rand() % pavardes.size()];
 
-            int kiek;
-            cout << "Iveskite kiek norite atsitiktinai sugeneruotu pazymiu: " << endl;
-            cin >> kiek;
+        int kiek;
+        cout << "Iveskite kiek norite atsitiktinai sugeneruotu pazymiu: " << endl;
+        cin >> kiek;
 
-            for (int j = 0; j < kiek; j++) {
-                int rng = rand() % 10 + 1;
-                A.paz.push_back(rng);
-                sum += rng;
-            }
-
-            A.egz = rand() % 10 + 1;
-
-            int n = A.paz.size();
-            if (n > 0) {
-                A.vid = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
-            }
-
-            sort(A.paz.begin(), A.paz.end());
-            if (n % 2 != 0) {
-                A.med = A.paz[n / 2];
-                A.med = A.med * 0.4 + A.egz * 0.6;
-            }
-            else if (n > 0) {
-                A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
-                A.med = A.med * 0.4 + A.egz * 0.6;
-
-            }
-            grupe.push_back(A);
+        for (int j = 0; j < kiek; j++) {
+            int rng = rand() % 10 + 1;
+            A.paz.push_back(rng);
+            sum += rng;
         }
+
+        A.egz = rand() % 10 + 1;
+
+        int n = A.paz.size();
+        if (n > 0) {
+            A.vid = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
+        }
+
+        sort(A.paz.begin(), A.paz.end());
+        if (n % 2 != 0) {
+            A.med = A.paz[n / 2];
+            A.med = A.med * 0.4 + A.egz * 0.6;
+        }
+        else if (n > 0) {
+            A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+            A.med = A.med * 0.4 + A.egz * 0.6;
+
+        }
+        grupe.push_back(A);
+    }
 }
 
-void outputas(const vector<Studentas>& grupe, char &rez) {
+void outputas(const vector<Studentas>& grupe, char& rez) {
     cout << "Rezultata isvesti su mediana(irasyti M arba m) ar vidurkiu(irasyti V arba v): " << endl;
-    cin >> rez;
     while (true) {
+        cin >> rez;
         if (rez == 'V' || rez == 'v' || rez == 'M' || rez == 'm') {
             break;
         }
@@ -246,4 +254,63 @@ void outputas(const vector<Studentas>& grupe, char &rez) {
             cout << setw(10) << fixed << setprecision(2) << A.med << endl;
         }
     }
+}
+void skaityti(vector <Studentas>& grupe, string failas)
+{
+    cout << "Iveskite failo pavadinima: ";
+    cin >> failas;
+
+    ifstream in(failas);
+    string line;
+    getline(in, line);
+
+    while (true) {
+        Studentas A;
+        int sum = 0;
+
+        if (!(in >> A.vardas >> A.pavarde)) break;
+
+        A.paz.clear();
+        int x;
+
+        vector<int> visi;
+        while (in >> x)
+        {
+            visi.push_back(x);
+            if (in.peek() == '\n') break;
+
+        }
+        if (visi.size() == 0) continue;
+
+        A.egz == visi.back();
+        visi.pop_back();
+        A.paz == visi;
+
+        int n = A.paz.size();
+        for (int k : A.paz) sum += k;
+
+        if (n > 0) {
+            A.vid = (sum * 1.0) / (n * 1.0) * 0.4 + A.egz * 0.6;
+        }
+
+        sort(A.paz.begin(), A.paz.end());
+        if (n % 2 != 0) {
+            A.med = A.paz[n / 2];
+        }
+        else if (n > 0) {
+            A.med = (A.paz[n / 2 - 1] + A.paz[n / 2]) / 2.0;
+        }
+        A.med = A.med * 0.4 + A.egz * 0.6;
+
+        grupe.push_back(A);
+
+    }
+
+
+
+
+
+
+
+
 }
