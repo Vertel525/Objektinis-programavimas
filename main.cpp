d63506e027f6b2ae5ce54368286819353b1f8196
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-04-27 15:56:17 +0300

    funkcijos testuoti isivedimas

--- main.cpp
+++ main.cpp
@@ -6,3 +6,10 @@
 #include "funkcijos.h"
 
 using namespace std;
+
+template <typename Container>
+void testuoti(const string& failas) {
+
+    Container studentai;
+    Container vargsiukai;
+    Container kietiakai;
